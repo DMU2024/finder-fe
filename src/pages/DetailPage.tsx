d219cfacd4f8f923ff644a417750c3877f227771
b1uey0 <3_14km@naver.com>
2024-05-23 01:28:33 +0900

    style: detailPage 레이아웃

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,3 +1,17 @@
+import { makeStyles, tokens } from "@fluentui/react-components";
+
+import AddContent from "../components/Detail/Back";
+
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: tokens.colorNeutralBackground2,
+    minHeight: "100vh",
+    width: "100%"
+  }
+});
+
 function DetailPage() {
   return <div></div>;
 }
