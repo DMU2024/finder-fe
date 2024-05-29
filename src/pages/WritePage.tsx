56eca47046ab1809b6e27de81628caf34232d559
b1uey0 <3_14km@naver.com>
2024-05-29 15:30:32 +0900

    style: WritePage textArea 추가, button 추가

--- src/pages/WritePage.tsx
+++ src/pages/WritePage.tsx
@@ -1,5 +1,7 @@
+import { DefaultButton } from "@fluentui/react";
 import { makeStyles, tokens } from "@fluentui/react-components";
 
+import AddContent from "../components/Write/AddContent";
 import ImgSelect from "../components/Write/ImgSelect";
 import LostList from "../components/Write/LostList";
 
@@ -21,14 +23,35 @@ const useStyles = makeStyles({
   },
   content: {
     display: "flex",
-    gap: "32px",
-    marginBottom: "100px"
+    marginBottom: "30px"
   },
   left: {
     width: "45%"
   },
   right: {
     width: "55%"
+  },
+  submitButtonContainer: {
+    marginTop: "20px",
+    marginBottom: "50px",
+    display: "flex",
+    justifyContent: "flex-end"
+  },
+  submitButton: {
+    padding: "20px 30px",
+    backgroundColor: tokens.colorBrandBackground,
+    color: "white",
+    borderRadius: "4px",
+    fontSize: "16px",
+    boxShadow: tokens.shadow4,
+    transition: "background-color 0.3s",
+    marginRight: "30px",
+    "&:hover": {
+      backgroundColor: tokens.colorBrandBackgroundHover
+    },
+    "&:active": {
+      backgroundColor: tokens.colorBrandBackgroundPressed
+    }
   }
 });
 
@@ -44,6 +67,14 @@ function WritePage() {
         <div className={styles.right}>
           <LostList />
         </div>
+      </div>
+      <AddContent />
+      <div className={styles.submitButtonContainer}>
+        <DefaultButton
+          className={styles.submitButton}
+          text="Submit"
+          onClick={() => console.log("Form submitted!")}
+        />
       </div>
     </div>
   );
