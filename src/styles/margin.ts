d523e1f7889defa44c816b272ede83efe309bd39
b1uey0 <3_14km@naver.com>
2024-10-01 01:58:35 +0900

    chore: margin mobile.ver 추가

--- src/styles/margin.ts
+++ src/styles/margin.ts
@@ -1,4 +1,6 @@
 const headerHeight = "80px";
+const headerMobileHeight = "68px";
 const contentMargin = "66px";
+const contentMobileMargin = "40px";
 
-export { headerHeight, contentMargin };
+export { headerHeight, contentMargin, headerMobileHeight, contentMobileMargin };
