7dee2110e862b17ae3b1e07360ef3ee42eb98147
Seungwon Lee <seungwon.dev@gmail.com>
2024-10-20 12:53:40 +0900

    chore: brandColor 위치 변경

--- src/styles/color.ts
+++ src/styles/color.ts
@@ -3,4 +3,30 @@ const backgroundColor = "#D2D2D2";
 const skeletonColor = "#414141";
 const secondaryColor = "#D9D9D9";
 
-export { mainColor, backgroundColor, skeletonColor, secondaryColor };
+const brandColor = {
+  0: mainColor,
+  10: mainColor,
+  20: mainColor,
+  30: mainColor,
+  40: mainColor,
+  50: mainColor,
+  60: mainColor,
+  70: mainColor,
+  80: mainColor,
+  90: mainColor,
+  100: mainColor,
+  110: mainColor,
+  120: mainColor,
+  130: mainColor,
+  140: mainColor,
+  150: mainColor,
+  160: mainColor
+};
+
+export {
+  mainColor,
+  backgroundColor,
+  skeletonColor,
+  secondaryColor,
+  brandColor
+};
