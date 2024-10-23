e6c92886065f8bf2faacbea0441204022ab7441d
b1uey0 <3_14km@naver.com>
2024-10-24 05:46:11 +0900

    style: login Page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,9 +1,12 @@
-import { makeStyles } from "@fluentui/react-components";
+import { makeStyles, tokens } from "@fluentui/react-components";
 import { useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 
 import { getLoginURI, postLogin } from "../apis/user";
 import useAuthStore from "../stores/auth";
+import { mainColor } from "../styles/color";
+import { headerMobileHeight, headerHeight } from "../styles/margin";
+import { mobileWidth } from "../styles/size";
 
 const useStyles = makeStyles({
   root: {
@@ -11,7 +14,90 @@ const useStyles = makeStyles({
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
+    height: `calc(100vh - ${headerHeight})`,
+    [`@media (max-width: ${mobileWidth})`]: {
+      height: `calc(100vh - ${headerMobileHeight})`,
+    }
+  },
+  container: {
+    width: "60vw",
+    height: "80vh",
+    boxShadow: "0 0px 12px rgba(0, 0, 0, 0.1)",
+    borderRadius: "30px 30px 0px 0px",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "fixed",
+    bottom: 0,
+    [`@media (max-width: ${mobileWidth})`]: {
+      width: "100vw",
+      height: "100vh"
+    }
+  },
+  leftContainer: {
+    width: "40vw",
+    height: "100%",
+    backgroundColor: mainColor,
+    borderRadius: "30px 0px 0px 0px",
+    [`@media (max-width: ${mobileWidth})`]: {
+      display: "none"
+    }
+  },
+  rightContainer: {
+    width: "60vw",
+    height: "100%",
+    backgroundColor: tokens.colorNeutralBackground1,
+    borderRadius: "0px 30px 0px 0px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    [`@media (max-width: ${mobileWidth})`]: {
+      width: "100vw",
+      height: "100vh"
+    }
+  },
+  logo: {
+    fontSize: "20px",
+    fontWeight: "bold",
+    color: mainColor,
+    width: "auto",
+    marginBottom: "10px",
+  },
+  loginText: {
+    fontSize: "42px",
+    fontWeight: "bold",
+    color: tokens.colorNeutralForeground2,
+    width: "auto",
+    marginBottom: "30px",
+    [`@media (max-width: ${mobileWidth})`]: {
+      fontSize: "36px",
+    }
+  },
+  detailText: {
+    fontSize: "16px",
+    fontWeight: "bold",
+    color: tokens.colorNeutralForegroundDisabled,
+    width: "auto",
+    marginBottom: "5px"
+  },
+  detailTextStylePlus: {
+    fontSize: "12px",
+    fontWeight: "bold",
+    color: tokens.colorNeutralBackground3Pressed,
+    width: "auto",
+  },
+  image: {
+    cursor: "pointer",
+    filter: "grayscale(100%)",
+    width: "40%",
+    marginTop: "20px",
+    marginBottom: "10px",
+    [`@media (max-width: ${mobileWidth})`]: {
+      width: "70%",
+    }
   }
 });
 
@@ -47,13 +133,27 @@ function LoginPage() {
 
   return (
     <div className={styles.root}>
-      <img
-        src="/kakaoLogin.png"
-        style={{ cursor: "pointer" }}
-        onClick={() => {
-          kakaoLogin();
-        }}
-      />
+
+      <div className={styles.container}>
+        <div className={styles.leftContainer}>
+
+        </div>
+        <div className={styles.rightContainer}>
+          <div className={styles.logo}> Finder </div>
+          <div className={styles.loginText}> Login </div>
+          <div className={styles.detailText}> 아이디와 비밀번호 없이 로그인해보세요. </div>
+
+          <img
+            src="/kakaoLogin.png"
+            className={styles.image}
+            onClick={() => {
+              kakaoLogin();
+            }}
+          />
+
+          <div className={styles.detailTextStylePlus}> 채팅, 마이페이지 기능을 이용하기 위해서는 로그인이 필요합니다. </div>
+        </div>
+      </div>
     </div>
   );
 }
